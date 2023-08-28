7769f869ee7c7c462766cec8664962bf2d174a95
kaien <kaienkira@gmail.com>
2023-08-28 19:56:54 +0800

    work sync

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ if [ $? -ne 0 ]; then exit 1; fi
 source pyenv/bin/activate
 if [ $? -ne 0 ]; then exit 1; fi
 
-mkdir pytmp
+mkdir -p pytmp
 if [ $? -ne 0 ]; then exit 1; fi
 TMPDIR=pytmp pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 if [ $? -ne 0 ]; then exit 1; fi
